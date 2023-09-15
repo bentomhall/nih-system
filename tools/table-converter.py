ee20b9ccacc41a23b0f3a8690275cac9d5f4503a
Benjamin Hall <bentomhall@gmail.com>
2023-09-14 18:53:05 -0700

    more resolving references,, all classes semi-port

--- tools/table-converter.py
+++ tools/table-converter.py
@@ -1,3 +1,4 @@
+import sys
 def process(lines):
 	in_table = False
 	table_line = 0
@@ -5,6 +6,12 @@ def process(lines):
 	for line in lines:
 		line = line.replace("\\\\", "")
 		if r'begin{figure' in line or r'end{figure' in line:
+			continue
+		if r'\subparagraph' in line:
+			output.append('<h3>Quick Build</h3>\n')
+			continue
+		if line[0:3] == r'<b>':
+			output.append(f'<p>{line}</p>\n')
 			continue
 		if r'begin{DndTable}' in line:
 			in_table = True
@@ -31,10 +38,10 @@ def process(lines):
 	return output
 
 if __name__ == "__main__":
-	filename = './html/classes/priest.html'
-	output = './html/classes/priest_2.html'
+	filename = sys.argv[1]
+	output = sys.argv[2]
 	transformed = []
-	with open(filename, 'r') as ifile:
+	with open(filename, 'r', encoding='utf-8') as ifile:
 		transformed = process(ifile.readlines())
-	with open(output, 'w') as ofile:
+	with open(output, 'w', encoding='utf-8') as ofile:
 		ofile.writelines(transformed)

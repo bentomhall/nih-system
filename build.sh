80e7e379b95e1c29f2bbc5460f92ffe118435478
Benjamin Hall <bentomhall@gmail.com>
2025-11-30 17:57:42 -0800

    linux build chain

--- build.sh
+++ build.sh
@@ -11,4 +11,17 @@ if [ -n "$BUILD" ] || [ -z $(docker image ls -q dnd-latex:latest) ]; then
   docker build -t dnd-latex:latest ./tools/docker-build/
 fi
 
-docker run --rm -v $PWD/latex:/workdir/input -v $PWD/output:/workdir/output dnd-latex:latest $input
+fullPath="$PWD/latex/$input"
+
+if [ ! -f "$fullPath" ]; then
+  echo "No such file $fullPath"
+  exit 1
+fi
+
+filename=${$input%.*}
+
+if [ -f "$PWD/output/$filename.pdf" ]; then
+  rm "$PWD/output/$filename.*"
+fi
+
+docker run --rm -it -v $PWD/latex:/workdir/input -v $PWD/output:/workdir/output dnd-latex:latest $input

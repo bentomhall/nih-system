c272267bdfb54868831a8e876219e76d31f726d9
Benjamin Hall <ben.hall@active911.com>
2025-12-08 12:36:44 -0800

    fix linux build script

--- build.sh
+++ build.sh
@@ -7,9 +7,6 @@ if [ -z "$1" ]; then
   input='main.tex'
 fi
 
-if [ -n $BUILD ]; then
-  docker build -t admiralbenbo/dnd-latex:latest "$PWD/tools/docker-build/"
-
 fullPath="$PWD/latex/$input"
 
 if [ ! -f "$fullPath" ]; then
@@ -17,8 +14,6 @@ if [ ! -f "$fullPath" ]; then
   exit 1
 fi
 
-filename=${input%.*}
-
-rm -rf output/*
+rm -rf output/${input%.*}*
 
 docker run --rm -it -v "$PWD/latex:/workdir/input" -v "$PWD/output:/workdir/output" admiralbenbo/dnd-latex:latest $input
